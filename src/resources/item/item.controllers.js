63414736ff0afdfe16e32591a37091a6e788305c
TerryThreatt <terry.threatt@gmail.com>
2021-06-01 19:32:59 -0500

    solution

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,17 @@
-import { crudControllers } from '../../utils/crud'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
 import { Item } from './item.model'
 
-export default crudControllers(Item)
+const run = async () => {
+  await connect('mongodb://localhost:2701/api-test')
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+
+  console.log(item)
+}
+
+run()
+// export default crudControllers(Item)
