c90968d38f288565fd516fe76d4f1ba942b4c108
TerryThreatt <terry.threatt@gmail.com>
2021-06-02 20:49:01 -0500

    solution

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
-import express from 'express'
 import { json, urlencoded } from 'body-parser'
+import cors from 'cors'
+import express from 'express'
 import morgan from 'morgan'
 import config from './config'
-import cors from 'cors'
-import { connect } from './utils/db'
-import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
 import listRouter from './resources/list/list.router'
+import userRouter from './resources/user/user.router'
+import { protect, signin, signup } from './utils/auth'
+import { connect } from './utils/db'
 
 export const app = express()
 
@@ -17,9 +18,12 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+app.use('/api', protect)
 app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/list', listRouter)
+app.use('/api/signin', signin)
+app.use('/api/signup', signup)
 
 export const start = async () => {
   try {
